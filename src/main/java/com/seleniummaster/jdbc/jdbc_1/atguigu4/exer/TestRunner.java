a7455937c1a152a042a1e99a6a57888cc9f97834
UyCoder <ahmedg47262582@gmail.com>
2020-02-06 00:57:09 +0300

    This is code of JDBC

---
+++ src/main/java/com/seleniummaster/jdbc/jdbc_1/atguigu4/exer/TestRunner.java
@@ -0,0 +1,18 @@
+package com.seleniummaster.jdbc.jdbc_1.atguigu4.exer;
+
+/**
+ * @author Ahmet Bughra
+ * @create 2020-02-06-12:52 AM
+ * @email bughra1991@gmail.com
+ */
+public class TestRunner {
+    // بۇ exer1Test نىڭ نەتىجىسىنى كونسولدا چىقىرىش ئۈچۈن قۇرۇلدى.
+    // JUnit
+    // دە كونسۇل كۆزننكى ئېچىلمايدىكەن.
+
+    public static void main(String[] args) {
+        Exer1Test exer1Test = new Exer1Test();
+        exer1Test.testInsert();
+    }
+    // مۇۋاپپىقىيەتلىك ھالدا ساندانغا قوشۇلدى..
+}
