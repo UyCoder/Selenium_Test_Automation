2c1824e5a453c8dd09bf05f755a829ec25ad469d
UyCoder <ahmedg47262582@gmail.com>
2020-02-06 01:03:12 +0300

    This is code of JDBC

--- src/main/java/com/seleniummaster/jdbc/jdbc_1/atguigu4/exer/TestRunner.java
+++ src/main/java/com/seleniummaster/jdbc/jdbc_1/atguigu4/exer/TestRunner.java
@@ -6,13 +6,19 @@ package com.seleniummaster.jdbc.jdbc_1.atguigu4.exer;
  * @email bughra1991@gmail.com
  */
 public class TestRunner {
-    // بۇ exer1Test نىڭ نەتىجىسىنى كونسولدا چىقىرىش ئۈچۈن قۇرۇلدى.
+    // بۇ exer1Test ۋەexer2Test نىڭ نەتىجىسىنى كونسولدا چىقىرىش ئۈچۈن قۇرۇلدى.
     // JUnit
     // دە كونسۇل كۆزننكى ئېچىلمايدىكەن.
 
     public static void main(String[] args) {
-        Exer1Test exer1Test = new Exer1Test();
-        exer1Test.testInsert();
+        // ساندانغا مۇۋاپىقىەتلىك قوشۇلدى...
+//        Exer1Test exer1Test = new Exer1Test();
+//        exer1Test.testInsert();
+
+//  بۇمۇ مۇۋاپپىقىيەتلىك بولدى.
+        Exer2Test exer2Test = new Exer2Test();
+        exer2Test.testInsert();
+
     }
     // مۇۋاپپىقىيەتلىك ھالدا ساندانغا قوشۇلدى..
 }
